0bfcc320b3868e5cd7f0228881046feee19e4500
danielm <dm1905once@gmail.com>
2026-06-17 18:06:03 -0700

    Working phase1

--- Advanced RAG/task/main.py
+++ Advanced RAG/task/main.py
@@ -5,18 +5,31 @@ def clean_input_file(path: str):
     policies = []
     steps = []
     supports = []
+    clean_records = []
+
     with open(path, 'r') as file:
         input_file = json.load(file)
     for record in input_file['knowledge-base']:
+        clean_record = None
         if "question" in record:
-            questions.append(record)
+            clean_record = {"question": record["question"], "answer": record["answer"]}
+            questions.append(clean_record)
         elif "policy" in record:
-            policies.append(record)
+            clean_record = {"policy": record["policy"]}
+            policies.append(clean_record)
         elif "steps" in record:
-            steps.append(record)
-        elif "policy" in record:
-            supports.append(record)
-    print(len(questions))
+            clean_record = {"how-to" : record["steps"]}
+            steps.append(clean_record)
+        elif "support" in record:
+            clean_record = {"support": record["support"]}
+            supports.append(clean_record)
+        if clean_record:
+            clean_records.append(clean_record)
+
+    clean_file = { "knowledge-base": clean_records}
+    with open("knowledge_base_clean.json", "w") as file:
+        json.dump(clean_file, file, indent=4)
+
 
 # Phase 1
 clean_input_file('knowledge_base_noisy.json')
