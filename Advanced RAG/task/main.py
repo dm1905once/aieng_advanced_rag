b6430f65d01252775f5943867fabdb15848c1803
danielm <dm1905once@gmail.com>
2026-06-17 18:51:14 -0700

    Still working phase1

--- Advanced RAG/task/main.py
+++ Advanced RAG/task/main.py
@@ -1,4 +1,6 @@
 import json
+from langchain_text_splitters import RecursiveJsonSplitter
+from langchain_core.documents import Document
 
 def clean_input_file(path: str):
     questions = []
@@ -7,6 +9,7 @@ def clean_input_file(path: str):
     supports = []
     clean_records = []
 
+    # Clean and split file by type of entry
     with open(path, 'r') as file:
         input_file = json.load(file)
     for record in input_file['knowledge-base']:
@@ -26,6 +29,22 @@ def clean_input_file(path: str):
         if clean_record:
             clean_records.append(clean_record)
 
+    # Create langchain docs with metadata, 5 entries of each type per doc
+    # splitter = RecursiveJsonSplitter(max_chunk_size=3000)
+    documents = []
+    chunks = [questions[i:i + 5] for i in range(0, len(questions), 5)]
+    for chunk in chunks:
+        documents.append(Document(
+            page_content=chunk,
+            metadata={
+                    "category": "faq",
+                    "tags": ["frequently asked questions", "help", "general information"]
+                }
+        ))
+        # print(documents)
+        print("=====")
+
+    # Save clean records into new json file
     clean_file = { "knowledge-base": clean_records}
     with open("knowledge_base_clean.json", "w") as file:
         json.dump(clean_file, file, indent=4)
